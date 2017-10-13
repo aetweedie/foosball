32cdbb9cc77fbdc9b5a212b6b9a59be9a8f7b62b
Michael DiGuiseppi <michael.diguiseppi@echostar.com>
2017-10-13 08:45:22 -0600

    Added promises for data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var knex = require('../db/knex')
 var express = require('express');
 var router = express.Router();
+var Promise = require("bluebird");
 
 function Scores() {
   return knex('scores');
@@ -18,70 +19,56 @@ router.get('/', function(req, res, next) {
       aWinPer,
       totalGames,
       lastGames;
-  knex.schema.raw('select round(cast(avg(mike_score) as numeric), 2) from scores where mike_score < 10;').then( function (data){
-    mAvg = data.rows[0].round;
-  });
-  knex.schema.raw('select round(cast(avg(alan_score) as numeric), 2) from scores where alan_score < 10;').then( function (data){
-    aAvg = data.rows[0].round;
-  });
-  Scores().count('mike_score').where({
-    mike_score: 10
-  }).then((wins) => {
-    mWins = wins[0].count;
-  });
-  Scores().count('alan_score').where({
-    alan_score: 10
-  }).then((wins) => {
-    aWins = wins[0].count;
-  });
-  Scores().sum('mike_score').then((total) => {
-    mTotal = total[0].sum;
-  });
-  Scores().sum('alan_score').then((total) => {
-    aTotal = total[0].sum;
-  });
-  Scores().count().then((totalRows) => {
-    totalGames = totalRows[0].count;
-    if (aWins && totalGames) {
-      aWinPer = ((aWins / totalGames) * 100).toFixed(1);
-    }
-    if (mWins && totalGames) {
-      mWinPer = ((mWins / totalGames) * 100).toFixed(1);
-    }
-  });
-  Scores().orderBy('id', 'desc').limit(10).then((last10Games) => {
-    lastGames = last10Games;
-  })
 
-  if (mAvg && mTotal && mWins && mWinPer && aAvg && aTotal && aWins && aWinPer && totalGames && lastGames) {
+  let p1 = knex.schema.raw('select round(cast(avg(mike_score) as numeric), 2) from scores where mike_score < 10;').then( function (data){
+    return data.rows[0].round;
+  });
+  let p2 = knex.schema.raw('select round(cast(avg(alan_score) as numeric), 2) from scores where alan_score < 10;').then( function (data){
+    return data.rows[0].round;
+  });
+  let p3 = Scores().count('mike_score').where({ mike_score: 10 }).then((wins) => {
+    return wins[0].count;
+  });
+  let p4 = Scores().count('alan_score').where({ alan_score: 10 }).then((wins) => {
+    return wins[0].count;
+  });
+  let p5 = Scores().sum('mike_score').then((total) => {
+    return total[0].sum;
+  });
+  let p6 = Scores().sum('alan_score').then((total) => {
+    return total[0].sum;
+  });
+  let p7 = Scores().count().then((totalRows) => {
+    return totalRows[0].count;
+  });
+  let p8 = Scores().orderBy('id', 'desc').limit(10).then((last10Games) => {
+    return last10Games;
+  });
+  let p9 = Scores().select().then((allGames) => {
+    return allGames;
+  });
+
+
+  Promise.all([p1, p2, p3, p4, p5, p6, p7, p8, p9]).then((values) => {
+    if (values[2] && values[8]) {
+      mWinPer = ((values[2] / values[8].length) * 100).toFixed(1);
+    }
+    if (values[3] && values[8]) {
+      aWinPer = ((values[3] / values[8].length) * 100).toFixed(1);
+    }
     res.render('index', {
-      mAvg: mAvg,
-      mTotal: mTotal,
-      mWins: mWins,
-      mWinPer: mWinPer,
-      aAvg: aAvg,
-      aTotal: aTotal,
-      aWins: aWins,
-      aWinPer: aWinPer,
-      totalGames: totalGames,
-      lastGames: lastGames,
-    });
-  } else {
-    setTimeout(() => {
-      res.render('index', {
-        mAvg: mAvg,
-        mTotal: mTotal,
-        mWins: mWins,
-        mWinPer: mWinPer,
-        aAvg: aAvg,
-        aTotal: aTotal,
-        aWins: aWins,
-        aWinPer: aWinPer,
-        totalGames: totalGames,
-        lastGames: lastGames,
-      });
-    }, 2000);
-  }
+          mAvg: values[0],
+          mTotal: values[4],
+          mWins: values[2],
+          mWinPer: mWinPer,
+          aAvg: values[1],
+          aTotal: values[5],
+          aWins: values[3],
+          aWinPer: aWinPer,
+          totalGames: values[8],
+          lastGames: values[7],
+        });
+  });
 });
 
 router.get('/scores/add', function(req, res, next) {
@@ -97,6 +84,12 @@ router.post('/scores/add', function(req, res, next) {
       res.redirect('/');
     }
   )
+});
+
+router.get('/scores/all', function(req, res, next) {
+  Scores().select().then((data) => {
+    res.render('all_scores', { scores: data });
+  });
 });
 
 
