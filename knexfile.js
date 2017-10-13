f9e724e76dc692037bf3f2cb7b9b8c4628254e9e
Alan Tweedie <alan.tweedie@INSLE0055412MAC.local>
2017-10-13 11:07:05 -0600

    Stupid comma

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/foosball'
-  }
+  },
   production: {
     client: ‘pg’,
     connection: process.env.DATABASE_URL + '?ssl=true',
